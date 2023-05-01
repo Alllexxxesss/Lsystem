effc0e8a8019cf60d042cc8c911d0051c442158f
Tst <mymail@gmail.com>
2023-05-01 22:55:04 +0300

    just a test

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def fa(i):
+ print ("now i =",i, " address i is ",id (i))   
+ if i<0:
+  print ("i<0")   
+  return None
+ if i == 0:
+  print ("i=0")   
+  return 1
+ 
+ return i * fa(i-1)
+
+print (fa(5.6))
